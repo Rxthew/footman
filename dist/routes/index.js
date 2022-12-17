46fd61ad12d094c6b8a63b5bea8d25dd16459460
rxthew <richardtheumaster@gmail.com>
2022-12-18 00:12:27 +0100

    Add seePlayer route

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const playerController_1 = require("../controllers/playerController");
 const router = express_1.default.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
+router.get('/player/:firstName.:lastName.:nationality', playerController_1.seePlayer);
 exports.default = router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
+import { seePlayer } from '../controllers/playerController';
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/player/:firstName.:lastName.:nationality', seePlayer);
+
+
 
 export default router
