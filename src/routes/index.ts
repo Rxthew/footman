d89e4cba98d2500b121eb9928eafe0d154401f7c
rxthew <richardtheumaster@gmail.com>
2023-01-13 12:25:13 +0100

    Define routes and map to controllers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,32 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
-import { seePlayer } from '../controllers/playerController';
+import { postFormCreateCompetition, postFormUpdateCompetition, preFormCreateCompetition, preFormUpdateCompetition, seeCompetition } from '../controllers/competitionController';
+import { postFormCreatePlayer, postFormUpdatePlayer, preFormCreatePlayer, preFormUpdatePlayer, seePlayer } from '../controllers/playerController';
+import { postFormCreateTeam, postFormUpdateTeam, preFormCreateTeam, preFormUpdateTeam, seeTeam } from '../controllers/teamController';
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/competitions/new', preFormCreateCompetition);
+router.get('/competitions/edit', preFormUpdateCompetition);
+router.get('/competitions/:name_:code', seeCompetition);
+router.post('/competitions', postFormCreateCompetition);
+router.put('/competitions/:name_:code', postFormUpdateCompetition);
+
+router.get('/player/new', preFormCreatePlayer);
+router.get('/player/edit', preFormUpdatePlayer);
 router.get('/player/:firstName_:lastName_:code', seePlayer);
+router.post('/player', postFormCreatePlayer);
+router.put('/player/:firstName_:lastName_:code', postFormUpdatePlayer);
+
+router.get('/team/new', preFormCreateTeam);
+router.get('/team/edit', preFormUpdateTeam);
+router.get('/team/:name_:code', seeTeam);
+router.post('/team', postFormCreateTeam);
+router.put('/team/:name_:code', postFormUpdateTeam);
+
 
 
 
