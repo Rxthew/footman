ace0a00295dc21f546e3b61c27dfdc6c5f66d7a5
rxthew <richardtheumaster@gmail.com>
2023-02-17 20:06:45 +0100

    Implement route for delete methods

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import { seeHomepage } from '../controllers/indexController';
-import { competitionIndexData, postFormCreateCompetition, postFormUpdateCompetition, preFormCreateCompetition, preFormUpdateCompetition, seeCompetition, seeCompetitionIndex, setIndexDataCache } from '../controllers/competitionController';
-import { postFormCreatePlayer, postFormUpdatePlayer, preFormCreatePlayer, preFormUpdatePlayer, seePlayer } from '../controllers/playerController';
-import { postFormCreateTeam, postFormUpdateTeam, preFormCreateTeam, preFormUpdateTeam, seeTeam } from '../controllers/teamController';
+import { competitionIndexData, deleteCompetition, postFormCreateCompetition, postFormUpdateCompetition, preFormCreateCompetition, preFormUpdateCompetition, seeCompetition, seeCompetitionIndex, setIndexDataCache } from '../controllers/competitionController';
+import { deletePlayer, postFormCreatePlayer, postFormUpdatePlayer, preFormCreatePlayer, preFormUpdatePlayer, seePlayer } from '../controllers/playerController';
+import { deleteTeam, postFormCreateTeam, postFormUpdateTeam, preFormCreateTeam, preFormUpdateTeam, seeTeam } from '../controllers/teamController';
 
 const router = express.Router();
 
 router.get('/', seeHomepage);
 router.use('/competition/data', setIndexDataCache);
 
+router.delete('/competition/:name.:code', deleteCompetition);
 router.get('/competition/new', preFormCreateCompetition);
 router.get('/competition/:name.:code/edit', preFormUpdateCompetition);
 router.get('/competition/:name.:code', seeCompetition);
@@ -17,13 +18,14 @@ router.get('/competition/index',seeCompetitionIndex);
 router.post('/competition/new', postFormCreateCompetition);
 router.put('/competition/:name.:code/edit', postFormUpdateCompetition);
 
-
+router.delete('/player/:firstName.:lastName.:code', deletePlayer);
 router.get('/player/new', preFormCreatePlayer);
 router.get('/player/:firstName.:lastName.:code/edit', preFormUpdatePlayer);
 router.get('/player/:firstName.:lastName.:code', seePlayer);
 router.post('/player/new', postFormCreatePlayer);
 router.put('/player/:firstName.:lastName.:code/edit', postFormUpdatePlayer);
 
+router.delete('/team/:name.:code', deleteTeam);
 router.get('/team/new', preFormCreateTeam);
 router.get('/team/:name.:code/edit', preFormUpdateTeam);
 router.get('/team/:name.:code', seeTeam);
