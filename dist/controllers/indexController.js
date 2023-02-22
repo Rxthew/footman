8536b17849f965845a655611663ecd085d7d0967
rxthew <richardtheumaster@gmail.com>
2023-02-22 19:33:59 +0100

    Update controller to facilitate unassigned queries

--- dist/controllers/indexController.js
+++ dist/controllers/indexController.js
@@ -31,35 +31,72 @@ let seeHomepageResults = resultsGenerator.seeHomepage();
 const { transactionWrapper } = queryHelpers;
 const seeHomepageCb = async function (t) {
     const seeHomepageQuery = async function () {
-        const { getFeaturedCompetitionUrls, getFeaturedPlayerUrls, getFeaturedTeamUrls } = queryHelpers;
-        const featuredCompetitionNames = ['English Premier League'];
-        const featuredPlayerNames = [['Kevin', 'De Bruyne']];
-        const featuredTeamNames = ['Manchester City'];
-        const featuredCompetitionsUrls = await getFeaturedCompetitionUrls(t, featuredCompetitionNames).catch((err) => { throw err; });
-        const featuredPlayersUrls = await getFeaturedPlayerUrls(t, featuredPlayerNames).catch((err) => { throw err; });
-        const featuredTeamsUrls = await getFeaturedTeamUrls(t, featuredTeamNames).catch((err) => { throw err; });
-        let featuredCompetitions = [];
-        let featuredPlayers = [];
-        let featuredTeams = [];
-        featuredCompetitionsUrls && featuredCompetitionNames.length === featuredCompetitionsUrls.length ? featuredCompetitionNames.forEach((competitionName, index) => {
-            featuredCompetitions = [...featuredCompetitions, { name: competitionName, url: featuredCompetitionsUrls[index] }];
-        }) : featuredCompetitions;
-        featuredPlayersUrls && featuredPlayerNames.length === featuredPlayersUrls.length ? featuredPlayerNames.forEach((playerNames, index) => {
-            featuredPlayers = [...featuredPlayers, { names: playerNames, url: featuredPlayersUrls[index] }];
-        }) : featuredPlayers;
-        featuredTeamsUrls && featuredTeamNames.length === featuredTeamsUrls.length ? featuredTeamNames.forEach((teamName, index) => {
-            featuredTeams = [...featuredTeams, { name: teamName, url: featuredTeamsUrls[index] }];
-        }) : featuredTeams;
+        const { getAllCompetitionUrlParams, getAllPlayerUrlParams, getAllTeamUrlParams, getFeaturedCompetitionUrls, getFeaturedPlayerUrls, getFeaturedTeamUrls, getAllDissociatedCompetitions, getAllDissociatedPlayers, getAllDissociatedTeams, } = queryHelpers;
+        const getFeaturedUrls = async function () {
+            const featuredCompetitionNames = ['English Premier League'];
+            const featuredPlayerNames = [['Kevin', 'De Bruyne']];
+            const featuredTeamNames = ['Manchester City'];
+            const featuredCompetitionsUrls = await getFeaturedCompetitionUrls(t, featuredCompetitionNames).catch((err) => { throw err; });
+            const featuredPlayersUrls = await getFeaturedPlayerUrls(t, featuredPlayerNames).catch((err) => { throw err; });
+            const featuredTeamsUrls = await getFeaturedTeamUrls(t, featuredTeamNames).catch((err) => { throw err; });
+            let featuredCompetitions = [];
+            let featuredPlayers = [];
+            let featuredTeams = [];
+            featuredCompetitionsUrls && featuredCompetitionNames.length === featuredCompetitionsUrls.length ? featuredCompetitionNames.forEach((competitionName, index) => {
+                featuredCompetitions = [...featuredCompetitions, { name: competitionName, url: featuredCompetitionsUrls[index] }];
+            }) : featuredCompetitions;
+            featuredPlayersUrls && featuredPlayerNames.length === featuredPlayersUrls.length ? featuredPlayerNames.forEach((playerNames, index) => {
+                featuredPlayers = [...featuredPlayers, { names: playerNames, url: featuredPlayersUrls[index] }];
+            }) : featuredPlayers;
+            featuredTeamsUrls && featuredTeamNames.length === featuredTeamsUrls.length ? featuredTeamNames.forEach((teamName, index) => {
+                featuredTeams = [...featuredTeams, { name: teamName, url: featuredTeamsUrls[index] }];
+            }) : featuredTeams;
+            return {
+                featuredCompetitions,
+                featuredPlayers,
+                featuredTeams
+            };
+        };
+        const getDissociatedUrls = async function () {
+            const freeCompetitionModels = await getAllDissociatedCompetitions(t).catch((err) => { throw err; });
+            const freePlayerModels = await getAllDissociatedPlayers(t).catch((err) => { throw err; });
+            const freeTeamModels = await getAllDissociatedTeams(t).catch((err) => { throw err; });
+            const competitionNames = freeCompetitionModels.map(competition => competition.getDataValue('name'));
+            const playerNames = freePlayerModels.map(player => [player.getDataValue('firstName'), player.getDataValue('lastName')]);
+            const teamNames = freeTeamModels.map(team => team.getDataValue('name'));
+            const competitionUrls = getAllCompetitionUrlParams(freeCompetitionModels, ['name', 'code']);
+            const playerUrls = getAllPlayerUrlParams(freePlayerModels, ['firstName', 'lastName', 'code']);
+            const teamUrls = getAllTeamUrlParams(freeTeamModels, ['name', 'code']);
+            let freeCompetitions = [];
+            let freePlayers = [];
+            let freeTeams = [];
+            competitionNames.length === competitionUrls.length ? competitionNames.forEach((competitionName, index) => {
+                freeCompetitions = [...freeCompetitions, { name: competitionName, url: competitionUrls[index] }];
+            }) : freeCompetitions;
+            playerNames.length === playerUrls.length ? playerNames.forEach((allNames, index) => {
+                freePlayers = [...freePlayers, { names: allNames, url: playerUrls[index] }];
+            }) : freePlayers;
+            teamNames.length === teamUrls.length ? teamNames.forEach((teamName, index) => {
+                freeTeams = [...freeTeams, { name: teamName, url: teamUrls[index] }];
+            }) : freeTeams;
+            return {
+                freeCompetitions,
+                freePlayers,
+                freeTeams
+            };
+        };
         return {
-            featuredCompetitions,
-            featuredPlayers,
-            featuredTeams
+            ...await getFeaturedUrls(),
+            ...await getDissociatedUrls()
         };
     };
     const results = await seeHomepageQuery().catch((err) => { throw err; });
+    console.log('____________________________________________________');
+    console.log(results);
+    console.log('____________________________________________________');
     const populateSeeHomepageResults = function () {
         if (results.featuredCompetitions && results.featuredPlayers && results.featuredTeams) {
-            Object.assign(seeHomepageResults, { featuredCompetitions: results.featuredCompetitions }, { featuredPlayers: results.featuredPlayers }, { featuredTeams: results.featuredTeams });
+            Object.assign(seeHomepageResults, { featuredCompetitions: results.featuredCompetitions }, { featuredPlayers: results.featuredPlayers }, { featuredTeams: results.featuredTeams }, { freeCompetitions: results.freeCompetitions, freePlayers: results.freePlayers, freeTeams: results.freeTeams });
         }
         else {
             const err = new Error('Query regarding homepage returned invalid data.');
--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -9,49 +9,98 @@ const { transactionWrapper} = queryHelpers;
 
 const seeHomepageCb = async function(t:Transaction){
 
-
     const seeHomepageQuery = async function(){
-        const { getFeaturedCompetitionUrls, getFeaturedPlayerUrls, getFeaturedTeamUrls} = queryHelpers;
+        const { getAllCompetitionUrlParams, getAllPlayerUrlParams, getAllTeamUrlParams,
+              getFeaturedCompetitionUrls, getFeaturedPlayerUrls, getFeaturedTeamUrls,  
+              getAllDissociatedCompetitions, getAllDissociatedPlayers, getAllDissociatedTeams,} = queryHelpers;
+
+        const getFeaturedUrls = async function(){
+            const featuredCompetitionNames = ['English Premier League'];
+            const featuredPlayerNames = [ ['Kevin', 'De Bruyne'] ];
+            const featuredTeamNames = ['Manchester City'];
     
-        const featuredCompetitionNames = ['English Premier League'];
-        const featuredPlayerNames = [ ['Kevin', 'De Bruyne'] ];
-        const featuredTeamNames = ['Manchester City'];
+            const featuredCompetitionsUrls = await getFeaturedCompetitionUrls(t,featuredCompetitionNames).catch((err:Error) => {throw err});
+            const featuredPlayersUrls = await getFeaturedPlayerUrls(t,featuredPlayerNames).catch((err:Error) => {throw err});
+            const featuredTeamsUrls = await getFeaturedTeamUrls(t, featuredTeamNames).catch((err:Error) => {throw err});
+    
+            let featuredCompetitions: {name: string, url:string}[] = [];
+            let featuredPlayers: {names: string[], url:string}[] = [];
+            let featuredTeams: {name: string, url:string}[] =[];
+    
+            featuredCompetitionsUrls && featuredCompetitionNames.length === featuredCompetitionsUrls.length ? featuredCompetitionNames.forEach((competitionName,index) => {
+                featuredCompetitions = [...featuredCompetitions, {name: competitionName, url: featuredCompetitionsUrls[index]}]
+            }) : featuredCompetitions;
+    
+            featuredPlayersUrls && featuredPlayerNames.length === featuredPlayersUrls.length ? featuredPlayerNames.forEach((playerNames,index) => {
+                featuredPlayers = [...featuredPlayers, {names: playerNames, url: featuredPlayersUrls[index]}]
+            }) : featuredPlayers;
+    
+            featuredTeamsUrls && featuredTeamNames.length === featuredTeamsUrls.length ? featuredTeamNames.forEach((teamName,index) => {
+                featuredTeams = [...featuredTeams, {name: teamName, url: featuredTeamsUrls[index]}]
+            }): featuredTeams;
 
-        const featuredCompetitionsUrls = await getFeaturedCompetitionUrls(t,featuredCompetitionNames).catch((err:Error) => {throw err});
-        const featuredPlayersUrls = await getFeaturedPlayerUrls(t,featuredPlayerNames).catch((err:Error) => {throw err});
-        const featuredTeamsUrls = await getFeaturedTeamUrls(t, featuredTeamNames).catch((err:Error) => {throw err});
+            return {
+                featuredCompetitions,
+                featuredPlayers,
+                featuredTeams
+                
+            }
 
-        let featuredCompetitions: {name: string, url:string}[] = [];
-        let featuredPlayers: {names: string[], url:string}[] = [];
-        let featuredTeams: {name: string, url:string}[] =[];
+        };
 
-        featuredCompetitionsUrls && featuredCompetitionNames.length === featuredCompetitionsUrls.length ? featuredCompetitionNames.forEach((competitionName,index) => {
-            featuredCompetitions = [...featuredCompetitions, {name: competitionName, url: featuredCompetitionsUrls[index]}]
-        }) : featuredCompetitions;
+        const getDissociatedUrls = async function(){
+            const freeCompetitionModels = await getAllDissociatedCompetitions(t).catch((err:Error) => {throw err});
+            const freePlayerModels = await getAllDissociatedPlayers(t).catch((err:Error) => {throw err});
+            const freeTeamModels = await getAllDissociatedTeams(t).catch((err:Error) => {throw err});
 
-        featuredPlayersUrls && featuredPlayerNames.length === featuredPlayersUrls.length ? featuredPlayerNames.forEach((playerNames,index) => {
-            featuredPlayers = [...featuredPlayers, {names: playerNames, url: featuredPlayersUrls[index]}]
-        }) : featuredPlayers;
+            const competitionNames = freeCompetitionModels.map(competition => competition.getDataValue('name'));
+            const playerNames = freePlayerModels.map(player => [player.getDataValue('firstName'), player.getDataValue('lastName')]);
+            const teamNames = freeTeamModels.map(team => team.getDataValue('name'));
 
-        featuredTeamsUrls && featuredTeamNames.length === featuredTeamsUrls.length ? featuredTeamNames.forEach((teamName,index) => {
-            featuredTeams = [...featuredTeams, {name: teamName, url: featuredTeamsUrls[index]}]
-        }): featuredTeams;
+            const competitionUrls = getAllCompetitionUrlParams(freeCompetitionModels,['name','code']);
+            const playerUrls = getAllPlayerUrlParams(freePlayerModels,['firstName','lastName','code']);
+            const teamUrls = getAllTeamUrlParams(freeTeamModels,['name','code']);
+            
+            let freeCompetitions: {name: string, url:string}[] = [];
+            let freePlayers: {names: string[], url:string}[] = [];
+            let freeTeams: {name: string, url:string}[] =[];
+    
+            competitionNames.length === competitionUrls.length ? competitionNames.forEach((competitionName,index) => {
+            freeCompetitions = [...freeCompetitions, {name: competitionName, url: competitionUrls[index]}]
+            }) : freeCompetitions;
+    
+            playerNames.length === playerUrls.length ? playerNames.forEach((allNames,index) => {
+                freePlayers = [...freePlayers, {names: allNames, url: playerUrls[index]}]
+            }) : freePlayers;
+    
+            teamNames.length === teamUrls.length ? teamNames.forEach((teamName,index) => {
+                freeTeams = [...freeTeams, {name: teamName, url: teamUrls[index]}]
+            }): freeTeams;
+
+            return {
+                freeCompetitions,
+                freePlayers,
+                freeTeams
+            }
+
+
+        }  
 
         return {
-            featuredCompetitions,
-            featuredPlayers,
-            featuredTeams
-            
+            ...await getFeaturedUrls(),
+            ...await getDissociatedUrls()
         }
+        
 
     }
 
     const results = await seeHomepageQuery().catch((err:Error)=>{throw err});
-    
 
     const populateSeeHomepageResults = function(){
         if(results.featuredCompetitions && results.featuredPlayers && results.featuredTeams){ 
-              Object.assign(seeHomepageResults, {featuredCompetitions: results.featuredCompetitions}, {featuredPlayers: results.featuredPlayers},{featuredTeams: results.featuredTeams});
+              Object.assign(seeHomepageResults, {featuredCompetitions: results.featuredCompetitions}, {featuredPlayers: results.featuredPlayers},{featuredTeams: results.featuredTeams},
+                            {freeCompetitions: results.freeCompetitions, freePlayers: results.freePlayers, freeTeams: results.freeTeams}
+                );
 
         }
         else{
