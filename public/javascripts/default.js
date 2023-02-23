807fd47645492965ea27c6a2851c103223bb52d0
rxthew <richardtheumaster@gmail.com>
2023-02-23 23:50:14 +0100

    Add icons to header
    Work on structure of index
    
    Work on interactivity

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,15 +1,41 @@
-const addLinks = document.querySelector('.addLinks');
-addLinks.addEventListener('click', function(event){
-    if(event.target.classList.contains('Add')){
-        const reveal = document.querySelector('.revealAddLinks');
-        if(reveal){
-            reveal.classList.toggle('hideAddLinks',true);
-            reveal.classList.toggle('revealAddLinks',false);
-        }
-        else{
-            const hide = document.querySelector('.hideAddLinks');
-            hide.classList.toggle('revealAddLinks',true);
-            hide.classList.toggle('hideAddLinks',false);
-        }
+const toggleGold = function(){
+    const path = document.querySelector('.add');
+    if(document.querySelector('.revealAddLinks')){
+        path.classList.toggle('gold',true);
     }
+    else{
+        path.classList.toggle('gold',false);
+    }
+}
+
+const toggleRotate = function(){
+    const path = document.querySelector('.add');
+    const svg = path.parentElement;
+    if(svg.classList.contains('fwdrotate')){
+        svg.classList.toggle('fwdrotate',false)
+        svg.classList.toggle('bckwdrotate',true)
+    }
+    else{
+        svg.classList.toggle('bckwdrotate',false)
+        svg.classList.toggle('fwdrotate',true)
+
+    }
+    
+
+}
+
+const addLinks = document.querySelector('.addLinks');
+addLinks.addEventListener('click', function(){
+    const reveal = document.querySelector('.revealAddLinks');
+    if(reveal){
+        reveal.classList.toggle('hideAddLinks',true);
+        reveal.classList.toggle('revealAddLinks',false);
+    }
+    else{
+        const hide = document.querySelector('.hideAddLinks');
+        hide.classList.toggle('revealAddLinks',true);
+        hide.classList.toggle('hideAddLinks',false);
+    }
+    toggleGold()
+    toggleRotate()
 })
